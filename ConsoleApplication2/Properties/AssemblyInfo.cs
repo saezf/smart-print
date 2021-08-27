00036ab7f6e0d39ac3bc1993eb9daf5125e8afae
Francisco Saez <fsaez@outlook.com>
2021-08-26 21:43:15 -0300

    Remove unused imports

--- ConsoleApplication2/Properties/AssemblyInfo.cs
+++ ConsoleApplication2/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication2")]
+[assembly: AssemblyTitle("SmartPrintUtil")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nodoplus")]
-[assembly: AssemblyProduct("ConsoleApplication2")]
+[assembly: AssemblyProduct("SmartPrintUtil")]
 [assembly: AssemblyCopyright("Copyright © Nodoplus 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
